ba2d2ab217869575b013621ede2c1dfa5a36dc65
AbigailSc <abigailsarzuri@gmail.com>
2022-09-03 14:24:38 -0300

    chore: Agegando lodash

---
+++ kimchechallenge/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h2>
+        My first Apollo app
+        <span role="img" aria-label="Rocket">
+          🚀
+        </span>
+      </h2>
+    </>
+  )
+}
