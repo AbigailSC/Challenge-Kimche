33a5f5f183187bd43df0732445f30f90c9fbd1c6
AbigailSc <abigailsarzuri@gmail.com>
2022-09-04 19:49:26 -0300

    Styles: Cambio propiedades de componentes

--- kimchechallenge/src/styles/globalStyles.js
+++ kimchechallenge/src/styles/globalStyles.js
@@ -1,21 +1,34 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { getColor } from "../utils/generateRandomColors"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #FAFBF7;
+    color: #0A1F28;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  }
+`
 
 export const Flex = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0 15%;
+  margin: 20px 15%;
   padding: 10px 25px;
 `;
 
 export const Heading = styled.h1`
-  // font-size: 44px;
   font-size: ${(props) => props.fontsize || "44px"};
-  // background-color: red;
 `;
 export const Text = styled.p`
-  font-size: 22px;
-  color: #718096;
-  // background-color: red;
+  font-size: ${(props) => props.fontsize || "22px"};
+  color: ${(props) => props.color || null};
+  font-weight: ${(props) => props.fontWeight || null};
+  margin: ${(props) => props.m || "20px 0"};
 `;
 export const Input = styled.input`
   font-size: 22px;
@@ -28,8 +41,7 @@ export const Input = styled.input`
   :focus {
     outline: none;
   }
-  background-color: red;
-  color: white;
+  background-color: #EDF2F7;
 `;
 export const Icon = styled.img`
   padding: 10px;
@@ -37,7 +49,7 @@ export const Icon = styled.img`
   border: none;
   border-top-right-radius: 15px;
   border-bottom-right-radius: 15px;
-  background-color: green;
+  background-color: #0A1F28;
 `;
 
 export const HStack = styled.div`
@@ -45,6 +57,7 @@ export const HStack = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: ${(props) => props.justifyContent || "flex-start"};
+  width: 100%;
 `;
 
 export const Container = styled.section`
@@ -53,8 +66,8 @@ export const Container = styled.section`
   height: 60px;
   justify-content: space-around;
   align-items: center;
-  background-color: blue;
-  border-radius: 15px;
+  background-color: #0A1F28;
+  border-radius: 25px;
 `
 
 export const Label = styled.label`
@@ -66,25 +79,50 @@ export const Label = styled.label`
   width: 100px;
   height: 40px;
   text-align: center;
-  border-radius: 15px;
   overflow: hidden;
   transition: linear 0.3s;
+
   color: #6e6e6edd;
+  border-radius: 25px;
+  font-weight: bolder;
+  padding: 5px;
 `
 
 export const RadioButton = styled.input`
   appearance: none;
   display: none;
   &:checked + ${Label} {
-  background-color: #1e90ff;
-  color: #f1f3f5;
-  transition: 0.3s;
-  border-radius: 10px;
-}
+    background-color: #A0AEC0;
+    color: #f1f3f5;
+    transition: 0.3s;
+    border-radius: 20px;
+  }
 `;
 
 export const Box = styled.div`
   display: flex;
-  flex-direction: row;
-  background-color: #E2E8F0;
+  flex-direction: column;
+  background-color: ${getColor};
+  padding: 30px 40px;
+  width: 340px;
+  height: 280px;
+  border-radius: 5%;
+  align-items: flex-start;
+  justify-content: center;
+  box-shadow: 0px 30px 45px 0px rgba(22,22,22,0.10);
+`
+
+export const Badge = styled.div`
+  padding: 15px;
+  border-radius: 15px;
+  background-color: ${getColor};
+  font-weight: ${(props) => props.fontWeight || null};
+  filter: brightness(80%);
+`
+
+export const Hr = styled.hr`
+  height: 1px;
+  color: green;
+  width: 100%;
+  border: none;
 `
