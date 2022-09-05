b402a293f8f65246cb5f89235eb8e1419b422e50
AbigailSc <abigailsarzuri@gmail.com>
2022-09-05 12:23:13 -0300

    feat: Agrego  renderizado de componentes card

--- kimchechallenge/src/components/Home/Home.jsx
+++ kimchechallenge/src/components/Home/Home.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
+import { Text } from "../../styles/globalStyles";
 import { CardCountry } from "../CardCountry/CardCountry";
 import { Searchbar } from "../Searchbar/Searchbar";
 import { SelectRadio } from "../SelectRadio/SelectRadio";
+import _ from "lodash";
 
 export const Home = () => {
   const [ findCountry, setFindCountry ] = useState(null)
+  const [ selection, setSelection ] = useState("continent")
+  // Declare el estado inicial en continent para que por defecto los agrupe por continente
   const handleSubmit = (e) => {
     setFindCountry(e)
   }
-  console.log("sera? ", findCountry)
+  const handleSelection = (e) => {
+    setSelection(e)
+  }
+  const groupDataBy = _.groupBy(findCountry, selection === "continent" ? "continent.name" : "languages[0].name")
+  console.log(groupDataBy)
   return (
   <>
     <Searchbar handleSubmit={handleSubmit} />
-    <SelectRadio />
-    <CardCountry
-      code = {findCountry?.code}
-      name = {findCountry?.name}
-      continent = {findCountry?.continent}
-      capital = {findCountry?.capital}
-      emoji = {findCountry?.emoji}
-      emojiU = {findCountry?.emojiU}
-      languages = {findCountry?.languages}
-    />
+    <SelectRadio handleSelection={handleSelection}/>
+    {
+      findCountry?.length
+      ? findCountry.map((element) => 
+        <CardCountry
+          key = {element?.code}
+          code = {element?.code}
+          name = {element?.name}
+          continent = {element?.continent}
+          capital = {element?.capital}
+          emoji = {element?.emoji}
+          emojiU = {element?.emojiU}
+          languages = {element?.languages}
+        />
+        )
+      : null
+    }
   </>
   )
 }
