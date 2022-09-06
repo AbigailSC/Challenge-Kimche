e0f4124dac6630354cefea8e3f764dd1b7c4533b
AbigailSc <abigailsarzuri@gmail.com>
2022-09-05 22:42:51 -0300

    refactor: Cambio condicional en el cardCountry

--- kimchechallenge/src/components/CardCountry/CardCountry.jsx
+++ kimchechallenge/src/components/CardCountry/CardCountry.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { Box, Flex, HStack, Text, Hr } from "../../styles/globalStyles";
 
-export const CardCountry = ( { name, code, continent, capital, languages, emoji, emojiU } ) => {
+export const CardCountry = ( { name, continent, capital, languages, emoji } ) => {
   return (
-    <Flex>
-      {!name && !code && !continent
-      ? <Text color="#718096">Ingrese un pais para ver los detalles</Text>
-      : (<Box>
-          <HStack justifyContent="space-between">
-            <Text color="#fff" fontWeight="bolder" fontsize="28px">{emoji} {name}</Text>
-            {/* <Badge fontWeight="bolder">
-              {code}
-            </Badge> */}
-          </HStack>
-          <Hr/>
-          <Text color="#fff" fontWeight="500">City: {capital}</Text>
-          <Text color="#fff" fontWeight="500">Languages: {languages[0].name}</Text>
-          <Text color="#fff" fontWeight="500">Continent: {continent.name}</Text>
-        </Box>)
-        }
+    <Flex margin="20px 0" padding="0">
+      <Box>
+        <HStack justifyContent="space-between">
+          <Text color="#718096" fontWeight="bolder" fontsize="28px">{emoji} {name}</Text>
+        </HStack>
+        <Hr/>
+        <Text color="#718096" fontWeight="400">City: {capital}</Text>
+        <Text color="#718096" fontWeight="400">Languages: {languages[0].name}</Text>
+        <Text color="#718096" fontWeight="400">Continent: {continent.name}</Text>
+      </Box>
     </Flex>
   )
 }
