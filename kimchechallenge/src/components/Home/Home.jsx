9a4464b3cc08411f28640f86f33e84fd7909476e
AbigailSc <abigailsarzuri@gmail.com>
2022-09-05 22:55:34 -0300

    feat: Agrego texto al renderizado

--- kimchechallenge/src/components/Home/Home.jsx
+++ kimchechallenge/src/components/Home/Home.jsx
@@ -1,40 +1,50 @@
 import React, { useState } from "react";
-import { Text } from "../../styles/globalStyles";
+import { Flex, Section, Text } from "../../styles/globalStyles";
 import { CardCountry } from "../CardCountry/CardCountry";
 import { Searchbar } from "../Searchbar/Searchbar";
 import { SelectRadio } from "../SelectRadio/SelectRadio";
 import _ from "lodash";
 
 export const Home = () => {
-  const [ findCountry, setFindCountry ] = useState(null)
-  const [ selection, setSelection ] = useState("continent")
+  const [ findCountry, setFindCountry ] = useState(null);
+  const [ selection, setSelection ] = useState("continent");
+  let countryIndex = [];
   // Declare el estado inicial en continent para que por defecto los agrupe por continente
   const handleSubmit = (e) => {
     setFindCountry(e)
-  }
+  };
   const handleSelection = (e) => {
     setSelection(e)
-  }
-  const groupDataBy = _.groupBy(findCountry, selection === "continent" ? "continent.name" : "languages[0].name")
-  console.log(groupDataBy)
+  };
+  const groupDataBy = _.groupBy(findCountry, selection === "continent" ? "continent.name" : "languages[0].name");
+  for (const property in groupDataBy) {
+    countryIndex.push(property)
+  };
   return (
   <>
     <Searchbar handleSubmit={handleSubmit} />
     <SelectRadio handleSelection={handleSelection}/>
     {
       findCountry?.length
-      ? findCountry.map((element) => 
-        <CardCountry
-          key = {element?.code}
-          code = {element?.code}
-          name = {element?.name}
-          continent = {element?.continent}
-          capital = {element?.capital}
-          emoji = {element?.emoji}
-          emojiU = {element?.emojiU}
-          languages = {element?.languages}
-        />
-        )
+      ? countryIndex?.map((ele, index) => 
+      <Flex key={index}>
+        <Text fontWeight="bolder" fontsize="28px">{ele}</Text>
+        <Section>
+          {groupDataBy[ele].map((element) => 
+            <CardCountry
+              key = {element?.code}
+              code = {element?.code}
+              name = {element?.name}
+              continent = {element?.continent}
+              capital = {element?.capital}
+              emoji = {element?.emoji}
+              emojiU = {element?.emojiU}
+              languages = {element?.languages}
+            />
+          )}
+        </Section>
+      </Flex>
+      )
       : null
     }
   </>
--- kimchechallenge/src/components/Searchbar/Searchbar.jsx
+++ kimchechallenge/src/components/Searchbar/Searchbar.jsx
@@ -3,14 +3,15 @@ import { Heading, Flex, Text, Input, Icon, HStack } from "../../styles/globalSty
 import iconSearch from "../../assets/iconSearch.svg";
 import { gql } from "apollo-boost";
 import { useQuery } from '@apollo/react-hooks';
+import { validateName } from "../../utils/validate";
 
 export const Searchbar = (props) => {
   const { handleSubmit } = props
   const [input, setInput] = useState({
     name: ""
-  })
-  const [country, setCountry] = useState("")
-  const [errors, setErrors] = useState({})
+  });
+  const [country, setCountry] = useState("");
+  const [errors, setErrors] = useState({});
   // Decidi no utilizar el filter de la query countries porque tendria que crear otra consulta para pasar el codigo al filtro y seria redundante dado que desde el filtrado en el componente ya puedo sacar los datos que necesito
   const GET_COUNTRIES = gql`
     query GET_COUNTRIES{
@@ -30,23 +31,13 @@ export const Searchbar = (props) => {
     }
   `
   const { loading, data } = useQuery(GET_COUNTRIES)
-  const regexValidateName = /^[a-zA-Z\s]+$/
-  const validateName = (input) => {
-    const errors = {}
-    if (input.name.length < 3) {
-      errors.name = "debe ser mayor de 2 caracteres"
-    } else if (!regexValidateName.test(input.name)) {
-      errors.name = "invalido, caracteres rancios"
-    }
-    return errors
-  }
   // Creo una funcion validadora para tener más control sobre el input
   const filterQueryCountry = (input) => {
     if (!loading) {
       const findCountry = data?.countries.filter((country) => country.name.toLowerCase().includes(input.name.toLowerCase()))
       return findCountry
     }
-  }
+  };
   const handleInput = (e) => {
     e.preventDefault();
     setInput({
@@ -56,16 +47,16 @@ export const Searchbar = (props) => {
       validateName({
         [e.target.name]: e.target.value
       })
-    )
-    
-  }
+    );
+  };
   const handleSearch = (e) => {
     e.preventDefault()
     if (!Object.keys(errors).length) {
       setCountry(filterQueryCountry(input));
       handleSubmit(country)
     }
-  }
+  };
+  // Decidi no utilizar Context para pasar la información del objeto de countries mediante props al componente padre porque solo lo hago en dos ocasiones y no me parecio necesario
   return (
   <Flex>
     <Heading>Country search 🗺️</Heading>
